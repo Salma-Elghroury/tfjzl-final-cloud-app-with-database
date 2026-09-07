c41779f00ca5fd2ff6e8655d51246cbe5afcc06b
Salma Elghroury <salmaelghroury@gmail.com>
2026-09-07 20:04:54 +0300

    Refactor models and add Question and Choice models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -11,11 +11,10 @@ import uuid
 
 
 # Instructor model
+
 class Instructor(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     full_time = models.BooleanField(default=True)
     total_learners = models.IntegerField()
 
@@ -24,27 +23,28 @@ class Instructor(models.Model):
 
 
 # Learner model
+
 class Learner(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     STUDENT = 'student'
     DEVELOPER = 'developer'
     DATA_SCIENTIST = 'data_scientist'
     DATABASE_ADMIN = 'dba'
+
     OCCUPATION_CHOICES = [
         (STUDENT, 'Student'),
         (DEVELOPER, 'Developer'),
         (DATA_SCIENTIST, 'Data Scientist'),
         (DATABASE_ADMIN, 'Database Admin')
     ]
+
     occupation = models.CharField(
         null=False,
         max_length=20,
         choices=OCCUPATION_CHOICES,
         default=STUDENT
     )
+
     social_link = models.URLField(max_length=200)
 
     def __str__(self):
@@ -53,7 +53,9 @@ class Learner(models.Model):
 
 
 # Course model
+
 class Course(models.Model):
+
     name = models.CharField(null=False, max_length=30, default='online course')
     image = models.ImageField(upload_to='course_images/')
     description = models.CharField(max_length=1000)
@@ -69,7 +71,9 @@ class Course(models.Model):
 
 
 # Lesson model
+
 class Lesson(models.Model):
+
     title = models.CharField(max_length=200, default="title")
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -79,25 +83,61 @@ class Lesson(models.Model):
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
 # And we could use the enrollment to track information such as exam submissions
+
 class Enrollment(models.Model):
+
     AUDIT = 'audit'
     HONOR = 'honor'
     BETA = 'BETA'
+
     COURSE_MODES = [
         (AUDIT, 'Audit'),
         (HONOR, 'Honor'),
         (BETA, 'BETA')
     ]
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+# Question Model
 
-# One enrollment could have multiple submission
+class Question(models.Model): 
+    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE) 
+    content = models.CharField(max_length=200) 
+    grade = models.IntegerField(default=50)
+
+    def str(self): return "Question: " + self.content
+
+# method to calculate if the learner gets the score of the question
+
+    def is_get_score(self, selected_ids):
+    
+    all_answers = self.choice_set.filter(is_correct=True).count()
+    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+    
+    if all_answers == selected_correct:
+        return True  
+    else:
+        return False
+
+# Choice Model
+
+class Choice(models.Model): 
+    
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    content = models.CharField(max_length=200) 
+    is_correct = models.BooleanField(default=False)
+
+
+# One enrollment could have multiple submissions
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+
+class Submission(models.Model):
+
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
